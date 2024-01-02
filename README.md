abfec9d0c5a024188d92b58bba1ecce73d8d0672
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-01-02 16:34:39 +0600

    responsive navbar photo added

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
-# React + Vite
+import { useState } from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { FaClipboardList } from "react-icons/fa";
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+const ResponsiveNav = () => {
 
-Currently, two official plugins are available:
+    const codeString = ` paste your code here `;
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+    const [copy,setCopy] = useState(false);
+
+    return (
+        <div className='bg-gray-500 max-w-5xl min-w-[25rem] rounded-md overflow-hidden'>
+            <div className='flex justify-between px-4 py-2 text-white '>
+                <p>Code</p>
+                {
+                    copy? <button>Copied !</button>:<button className='flex justify-center items-center gap-1' onClick={()=>{
+                    navigator.clipboard.writeText(codeString);  //For copy code
+                    setCopy(true)   //copy (true) hoise bujhate
+                    setTimeout(()=>{
+                        setCopy(false)  // 3s pore abar copy button show korte
+                    },5000)
+                }}><FaClipboardList /> Copy code</button>
+                }
+            </div>
+            <SyntaxHighlighter language="jsx" style={atomOneDark} customStyle={{
+                padding:'20px'
+            }}
+            // wrapLongLines={true}
+            >
+                {codeString}
+            </SyntaxHighlighter>
+        </div>
+    );
+};
+
+export default ResponsiveNav;
--- src/pages/Navbar/ResponsiveNav/ResponsiveNav.jsx
+++ src/pages/Navbar/ResponsiveNav/ResponsiveNav.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { FaClipboardList } from "react-icons/fa";
+import responsiveNav from '../../../assets/images/Navbar/responsiveNav.png'
 
 const ResponsiveNav = () => {
 
@@ -53,29 +54,34 @@ const ResponsiveNav = () => {
     export default Navbar;`;
 
 
-    const [copy,setCopy] = useState(false);
+    const [copy, setCopy] = useState(false);
 
     return (
-        <div className='bg-gray-500 max-w-5xl min-w-[25rem] rounded-md overflow-hidden'>
-            <div className='flex justify-between px-4 py-2 text-white '>
-                <p>Code</p>
-                {
-                    copy? <button>Copied !</button>:<button className='flex justify-center items-center gap-1' onClick={()=>{
-                    navigator.clipboard.writeText(codeString);  //For copy code
-                    setCopy(true)   //copy (true) hoise bujhate
-                    setTimeout(()=>{
-                        setCopy(false)  // 3s pore abar copy button show korte
-                    },5000)
-                }}><FaClipboardList /> Copy code</button>
-                }
+        <div>
+            <div>
+                <img src={responsiveNav} alt="" />
             </div>
-            <SyntaxHighlighter language="jsx" style={atomOneDark} customStyle={{
-                padding:'20px'
-            }}
-            // wrapLongLines={true}
-            >
-                {codeString}
-            </SyntaxHighlighter>
+            <div className='bg-gray-500 max-w-5xl min-w-[25rem] rounded-md overflow-hidden'>
+                <div className='flex justify-between px-4 py-2 text-white '>
+                    <p>Code</p>
+                    {
+                        copy ? <button>Copied !</button> : <button className='flex justify-center items-center gap-1' onClick={() => {
+                            navigator.clipboard.writeText(codeString);  //For copy code
+                            setCopy(true)   //copy (true) hoise bujhate
+                            setTimeout(() => {
+                                setCopy(false)  // 3s pore abar copy button show korte
+                            }, 5000)
+                        }}><FaClipboardList /> Copy code</button>
+                    }
+                </div>
+                <SyntaxHighlighter language="jsx" style={atomOneDark} customStyle={{
+                    padding: '20px'
+                }}
+                // wrapLongLines={true}
+                >
+                    {codeString}
+                </SyntaxHighlighter>
+            </div>
         </div>
     );
 };
